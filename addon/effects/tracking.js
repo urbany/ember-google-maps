c76b453b49a6a1edcd2cb02c62db8075378520e5
João Teixeira <jldteixeira@gmail.com>
2026-02-22 10:26:17 +0000

    refactor: remove unused Ember dependency and clean up untrack function

--- addon/effects/tracking.js
+++ addon/effects/tracking.js
@@ -2,13 +2,7 @@ import { createCache, getValue } from '@glimmer/tracking/primitives/cache';
 import { _backburner } from '@ember/runloop';
 import { registerDestructor } from '@ember/destroyable';
 
-// TODO Revisit when Glimmer gets effects
 let untrack = (fn) => fn();
-
-import Ember from 'ember';
-if (Object.keys(Ember.__loader.registry).includes('@glimmer/validator')) {
-  ({ untrack } = Ember.__loader.require('@glimmer/validator'));
-}
 
 /**
  * It’s been clear since launch that Octane’s design doesn’t account for a few
